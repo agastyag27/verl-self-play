bd9221733530328c85cd5d7e2151cd39d08e3980
Agastya Goel <goel.agastya@gmail.com>
2025-11-29 22:04:21 -0800

    removed cruft

--- recipe/debate/make_train.py
+++ recipe/debate/make_train.py
@@ -19,36 +19,6 @@ import os
 
 import datasets
 
-# add a row to each data item that represents a unique id
-def make_map_fn(split):
-    def process_fn(example, idx):
-        question_raw = example.pop("question")
-
-        question = question_raw + " " + instruction_following
-
-        answer_raw = example.pop("answer")
-        solution = extract_solution(answer_raw)
-        data = {
-            "data_source": data_source,
-            "prompt": [
-                {
-                    "role": "user",
-                    "content": question,
-                }
-            ],
-            "ability": "math",
-            "reward_model": {"style": "rule", "ground_truth": solution},
-            "extra_info": {
-                "split": split,
-                "index": idx,
-                "answer": answer_raw,
-                "question": question_raw,
-            },
-        }
-        return data
-
-    return process_fn
-
 data = [
     {
         "topic": "are apples or oranges the superior fruit?",
